ee990946b72c07bb7e699a334a29ef0e979bdaf1
dustin.luk <dustin.luk@flexsystem.com>
2018-04-06 14:31:09 +0800

    use meal-form and list-item into meals and meal component

--- src/health/meals/containers/meal/meal.component.ts
+++ src/health/meals/containers/meal/meal.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Meal, MealsService } from '../../../shared/services/meals/meals.service';
 
 @Component({
   selector: 'meal',
   styleUrls: ['meal.component.scss'],
   template: `
     <div class="meal">
-      I am a meal!
+      <div class="meal__title">
+        <h1>
+          <img src="/img/food.svg">
+          <span>Create meal</span>
+        </h1>
+      </div>
+      <div>
+        <meal-form
+          (create)="addMeal($event)">
+        </meal-form>
+      </div>
     </div>
   `
 })
 export class MealComponent {
-  constructor() {}
+  
+  constructor(
+    private mealsService: MealsService,
+    private router: Router
+  ) {}
+
+  async addMeal(event: Meal) {
+    await this.mealsService.addMeal(event);
+    // redirect
+    this.backToMeals();
+  }
+
+  backToMeals() {
+    this.router.navigate(['meals']);
+  }
+
 }
