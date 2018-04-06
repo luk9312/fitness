992e7e5bc703ab852e1a3c47bc99a30319554d0b
dustin.luk <dustin.luk@flexsystem.com>
2018-04-06 15:33:31 +0800

    add update function within meal component

--- src/health/meals/components/meal-form/meal-form.component.ts
+++ src/health/meals/components/meal-form/meal-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
 import { FormArray, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 import { Meal } from '../../../shared/services/meals/meals.service';
@@ -44,13 +44,22 @@ import { Meal } from '../../../shared/services/meals/meals.service';
             </label>
           </div>
         </div>
+
         <div class="meal-form__submit">
           <div>
             <button
               type="button"
               class="button"
+              *ngIf="!exists"
               (click)="createMeal()">
               Create meal
+            </button>
+            <button
+              type="button"
+              class="button"
+              *ngIf="exists"
+              (click)="updateMeal()">
+              Save
             </button>
             <a 
               class="button button--cancel"
@@ -58,15 +67,45 @@ import { Meal } from '../../../shared/services/meals/meals.service';
               Cancel
             </a>
           </div>
+
+          <div class="meal-form__delete" *ngIf="exists">
+            <div *ngIf="toggled">
+              <p>Delete  item?</p>
+              <button class="confirm" type="button" (click)="removeMeal()">
+                Yes
+              </button>
+              <button class="cancel" type="button" (click)="toggle()">
+                No
+              </button>
+            </div>
+
+            <button class="button button--delete" type="button" (click)="toggle()">
+              Delete
+            </button>
+
+          </div>
         </div>
       </form>
     </div>
   `
 })
-export class MealFormComponent {
+export class MealFormComponent implements OnChanges {
+
+  toggled = false;
+  exists = false;
+
+  @Input()
+  meal: Meal;
 
   @Output()
   create = new EventEmitter<Meal>();
+
+  @Output()
+  update = new EventEmitter<Meal>();
+
+  @Output()
+  remove = new EventEmitter<Meal>();
+
 
   form = this.fb.group({
     name: ['', Validators.required],
@@ -76,6 +115,28 @@ export class MealFormComponent {
   constructor(
     private fb: FormBuilder
   ) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(this.meal && this.meal.name) {
+      this.exists = true;
+      this.emptyIngredients();
+
+      const value = this.meal;
+      this.form.patchValue(value);
+
+      if (value.ingredients) {
+        for(const item of value.ingredients) {
+          this.ingredients.push(new FormControl(item));
+        }
+      }
+    }
+  }
+
+  emptyIngredients() {
+    while (this.ingredients.controls.length) {
+      this.ingredients.removeAt(0);
+    }
+  }
 
   get required() {
     return (
@@ -100,6 +161,20 @@ export class MealFormComponent {
     if (this.form.valid) {
       this.create.emit(this.form.value);
     }
+  }
+
+  updateMeal() {
+    if (this.form.valid) {
+      this.update.emit(this.form.value);
+    }
+  }
+
+  removeMeal() {
+    this.remove.emit(this.form.value);
+  }
+
+  toggle() {
+    this.toggled = !this.toggled;
   }
 
 }
--- src/health/meals/containers/meal/meal.component.ts
+++ src/health/meals/containers/meal/meal.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/switchMap';
 
 import { Meal, MealsService } from '../../../shared/services/meals/meals.service';
 
@@ -11,27 +15,67 @@ import { Meal, MealsService } from '../../../shared/services/meals/meals.service
       <div class="meal__title">
         <h1>
           <img src="/img/food.svg">
-          <span>Create meal</span>
+          <span *ngIf="meal$ | async as meal; else title;">
+          {{ meal.name ? 'Edit' : 'Create' }} meal
+          </span>
+          <ng-template #title>
+            Loading...
+          </ng-template>
         </h1>
       </div>
-      <div>
+      <div *ngIf="meal$ | async as meal; else loading">
         <meal-form
-          (create)="addMeal($event)">
+          [meal]="meal"
+          (create)="addMeal($event)"
+          (update)="updateMeal($event)"
+          (remove)="removeMeal($event)">
         </meal-form>
       </div>
+      <ng-template #loading>
+        <div class="message">
+          <img src="/img/loading.svg">
+          Fetching meal...
+        </div>
+      </ng-template>
     </div>
   `
 })
-export class MealComponent {
+export class MealComponent implements OnInit, OnDestroy {
+
+  meal$: Observable<Meal>;
+  subscription: Subscription;
   
   constructor(
     private mealsService: MealsService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
+
+  ngOnInit() {
+    this.subscription = this.mealsService.meals$.subscribe();
+    this.meal$ = this.route.params
+      .switchMap(param => this.mealsService.getMeal(param.id));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   async addMeal(event: Meal) {
     await this.mealsService.addMeal(event);
     // redirect
+    this.backToMeals();
+  }
+
+  async updateMeal(event: Meal) {
+    const key = this.route.snapshot.params.id;
+    await this.mealsService.updateMeal(key, event);
+    this.backToMeals();
+  }
+
+  async removeMeal (event: Meal) {
+    const key = this.route.snapshot.params.id;
+    await this.mealsService.removeMeal(key);
     this.backToMeals();
   }
 
