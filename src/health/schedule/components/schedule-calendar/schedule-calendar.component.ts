b044ce8883db2774910198d2332c951d9592e2c4
Dustin Luk <kingsan212@gmail.com>
2018-04-07 17:22:18 +0800

    edited error, fix from getDay to getDate

--- src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
+++ src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 import { ScheduleItem, ScheduleList } from '../../../shared/services/schedule/schedule.service';
 
@@ -7,42 +7,39 @@ import { ScheduleItem, ScheduleList } from '../../../shared/services/schedule/sc
   styleUrls: ['schedule-calendar.component.scss'],
   template: `
     <div class="calendar">
-
+      
       <schedule-controls
         [selected]="selectedDay"
         (move)="onChange($event)">
       </schedule-controls>
-
       <schedule-days
         [selected]="selectedDayIndex"
         (select)="selectDay($event)">
       </schedule-days>
-
       <schedule-section
         *ngFor="let section of sections"
         [name]="section.name"
         [section]="getSection(section.key)">
       </schedule-section>
-
     </div>
   `
 })
-export class ScheduleCalendarComponent implements OnChanges{
+export class ScheduleCalendarComponent implements OnChanges {
 
   selectedDayIndex: number;
   selectedDay: Date;
   selectedWeek: Date;
 
   sections = [
-    {key: 'morning', name: 'Morning'},
-    {key: 'lunch', name: 'Lunch'},
-    {key: 'evening', name: 'Evening'},
-    {key: 'snacks', name: 'Snacks and Drinks'}
-  ]
+    { key: 'morning', name: 'Morning' },
+    { key: 'lunch', name: 'Lunch' },
+    { key: 'evening', name: 'Evening' },
+    { key: 'snacks', name: 'Snacks and Drinks' },
+  ];
 
   @Input()
-  set date(date : Date) {
-    this.selectedDay =new Date(date.getTime());
+  set date(date: Date) {
+    this.selectedDay = new Date(date.getTime());
   }
 
   @Input()
@@ -53,22 +50,22 @@ export class ScheduleCalendarComponent implements OnChanges{
 
   constructor() {}
 
-  ngOnChanges(){
+  ngOnChanges() {
     this.selectedDayIndex = this.getToday(this.selectedDay);
     this.selectedWeek = this.getStartOfWeek(new Date(this.selectedDay));
   }
 
-  getSection (name: string): ScheduleItem {
+  getSection(name: string): ScheduleItem {
     return this.items && this.items[name] || {};
   }
 
-  selectDay(index: number){
+  selectDay(index: number) {
     const selectedDay = new Date(this.selectedWeek);
     selectedDay.setDate(selectedDay.getDate() + index);
     this.change.emit(selectedDay);
   }
 
-  onChange(weekOffset : number) {
+  onChange(weekOffset: number) {
     const startOfWeek = this.getStartOfWeek(new Date());
     const startDate = (
       new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate())
@@ -77,9 +74,9 @@ export class ScheduleCalendarComponent implements OnChanges{
     this.change.emit(startDate);
   }
 
-  private getToday(date : Date){
-    let today = date.getDate() - 1;
-    if( today < 0 ) {
+  private getToday(date: Date) {
+    let today = date.getDay() - 1;
+    if (today < 0) {
       today = 6;
     }
     return today;
@@ -87,7 +84,8 @@ export class ScheduleCalendarComponent implements OnChanges{
 
   private getStartOfWeek(date: Date) {
     const day = date.getDay();
-    const diff = date.getDay() - day + (day === 0? -6 : 1);
+    const diff = date.getDate() - day + (day === 0 ? -6 : 1);
     return new Date(date.setDate(diff));
   }
+
 }
